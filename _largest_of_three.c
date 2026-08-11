64a0eae3e7d4aec219cad51c14c0f3d445d41278
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-11 22:15:07 +0530

    Create _largest_of_three.c
    Find the largest of three numbers

---
+++ _largest_of_three.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+  int a,b,c;
+printf("enter the value pf a,b ,c:");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&b>c){
+printf("a is greatest=%d",a);
+}
+else if(b>c){
+printf("b is greatest=%d",b);
+}
+else{
+printf("c is greatest=%d",c);
+  }
+  }
+  
