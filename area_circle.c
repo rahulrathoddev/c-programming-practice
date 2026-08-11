878a97e97a87de4d9b96afefa110bce03210649c
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-11 12:23:03 +0530

    Create area_circle.c

---
+++ area_circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+  int radius,pi,area;
+pi=3.142;
+printf("enter the radius of circle:");
+scanf("%d",&radius);
+area=pi*radius*radius;
+
+
+printf("the area of circle=",area);
+}
