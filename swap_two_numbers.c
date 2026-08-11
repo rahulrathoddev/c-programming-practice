75311bf91f5e02c694dae77565eb2a5c6ff8c3e9
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-11 14:12:41 +0530

    Update swap_two_numbers.c
    swaping number, using temp(third variable) 
    swaping number, without third variable

--- swap_two_numbers.c
+++ swap_two_numbers.c
@@ -8,3 +8,21 @@ a=b;
 b=temp;
 printf("%d%d",a,b);
 }
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}
