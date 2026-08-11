a793aecca134fecff05cf37372d9f3737adaeb6a
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-11 12:16:53 +0530

    arithmetic_operations.c

---
+++ arithmetic_operations.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+  int a,b,add,sub,multi,div;
+printf("enter the value of  a  and b");
+scanf("%d%d,&a,&b);
+add=a+b;
+printf("sum is =",add);
+sub=a-b;
+
+printf("substraction of a and b is =",sub);
+sub=a*b;
+
+printf("multi of a and b is =",multi);
+sub=a/b;
+printf("div of  a and b is =",div);
+}
+
+
+
