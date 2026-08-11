fcf955dab5342c0661308550cce29c65380d7a13
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-11 12:27:41 +0530

    area_rectangle.c

---
+++ area_rectangle.c
@@ -0,0 +1,8 @@
+#Include<stdio.h>
+void main(){
+  int area,length,breadth;
+printf("enter lenght and breadth of rectangle");
+scanf("%d%d",&lengh,&breadth);
+area=length*breadth;
+printf("Area of rectangle is =:",area);
+}
