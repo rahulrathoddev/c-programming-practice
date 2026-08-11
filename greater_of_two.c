a2f2fb4e198f9640b261d3bcb7ae9dbbe180d6fb
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-11 14:38:35 +0530

    Create greater_of_two.c

---
+++ greater_of_two.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+  int a ,b;
+printf("enter value of a and b");
+scanf("%d%d",&a,&b);
+if(a>b){
+printf("a is greatest:%d",a);
+}
+else{
+printf("b is greatest:%d",b);
+}
+}
