63f84cacee501806b1ff9fdfcc4cebb5207d6087
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-11 14:25:19 +0530

    Create negative.c
    whether given number is postive or negative

---
+++ positive_/negative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+  int num;
+printf("enter a number:");
+scanf("%d",&num);
+if(num>o){
+printf("number is positive=%d",num);
+}
+  else{
+printf("number is negative=%d",num);
+  }
+}
