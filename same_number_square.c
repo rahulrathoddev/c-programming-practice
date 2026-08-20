b0ee59801c7d2c5185fb0aa5d42bc35501ec2179
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-20 23:26:11 +0530

    Create same_number_square.c

---
+++ same_number_square.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+  int n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+ int num=1;
+    for(int i=0;i<=n;i++){
+       
+    for(int j=0;j<=n;j++){
+            printf("%d",num);
+
+    }      
+    num+=1;
+    
+printf("\n");
+        }
+    
