870f738a91cbc117c214bb0adbe8a6de7720368f
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-20 23:05:48 +0530

    Create increasing_number_triangle.c

---
+++ increasing_number_triangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+
+    for(int i=0;i<=n;i++){
+        int num=1;
+    for(int j=0;j<i;j++){
+            printf("%d",num);
+            num++;
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
